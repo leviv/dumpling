0a3dd6e95294875d0c33784060b7f7a98f538c60
ayunahkim <79731952+ayunahkim@users.noreply.github.com>
2025-12-03 23:12:51 +0900

    styled landing page a little

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,34 @@
+<script>
+    import box from '$lib/assets/tempbox.png';
+    import boxgif from '$lib/assets/boxopening.gif';
+    import { goto } from '$app/navigation';
+
+    let myimg = box;
+
+    function changeImg(){
+        myimg = boxgif;
+        setTimeout(()=>{
+            goto('/map');
+        }, 2000)
+    }
+</script>
+
 <!-- landing page -->
-<h1>World Dumpling Index</h1>
-<a href="/map">Click to Enter</a>
+ <div id="landingstuff">
+    <h1>World Dumpling Index</h1>
+    <img id="box" alt="box with world dumpling index on it" src={myimg} on:click={changeImg}>
+    <a href="/map">Click to Enter</a>
+ </div>
+
+<style>
+    #box {
+        width:100%;
+        margin-bottom:20px;
+    }
+	#landingstuff {
+		margin: auto;
+        width: 50%;
+        padding-top:30%;
+        text-align:center;
+	}
+</style>
