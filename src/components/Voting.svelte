dd533180e2c81bfbb2db4622201b2bfb63d5cdc7
ayunahkim <79731952+ayunahkim@users.noreply.github.com>
2025-12-04 19:56:57 +0900

    created base for voting component

---
+++ src/components/Voting.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import dumplings from '$lib/dumplings.json';
+</script>
+
+<!-- put somewhere around sidebar dumpling cards -->
+<div class="voteContainer">
+    <p>Is this a dumpling?</p>
+    <button >yes!</button>
+    <button>no!</button>
+</div>
