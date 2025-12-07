cd92796f7b6836e7545efb81ae52ccd49eea3e7e
Levi Villarreal <villarreallevi@gmail.com>
2025-12-07 13:50:41 -0500

    fix voting images

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -16,30 +16,29 @@
 			{#each dumpling.tags as tag}
 				<li>{tag}</li>
 			{/each}
-			<img class="dumplingart" src="assets/dumplings/{dumpling.img}">
+			<img class="dumplingart" src="assets/dumplings/{dumpling.img}" alt="Dumpling art" />
 		</ul>
-		
 	</div>
 </div>
 
 <style>
-	li{
-		margin-left:20px;
+	li {
+		margin-left: 20px;
 	}
 
 	.container {
 		position: relative;
 		width: calc(497px * 0.6);
 		height: calc(300px * 0.6);
-		font-size:0.8rem;
-		line-height:0.8rem;
+		font-size: 0.8rem;
+		line-height: 0.8rem;
 	}
 
 	.content {
 		position: absolute;
 		top: 7%;
-		left:-1.2rem;
-		width:99%;
+		left: -1.2rem;
+		width: 99%;
 	}
 
 	.indexcard {
@@ -52,10 +51,10 @@
 	}
 
 	.dumplingart {
-		width:60px;
-		height:auto;
-		position:absolute;
-		top:-10px;
-		right:-10px;
+		width: 60px;
+		height: auto;
+		position: absolute;
+		top: -10px;
+		right: -10px;
 	}
 </style>
--- src/components/Voting.svelte
+++ src/components/Voting.svelte
@@ -1,49 +1,44 @@
-<script lang="ts">
-    import dumplings from '$lib/dumplings.json';
-</script>
-
 <div class="container">
-    <div class="votecard">
-        <p>Is this a dumpling?</p>
-        <button><img src="./assets/yes.png" alt="yes!" title="yes!"></button>
-        <button><img src="./assets/no.png" alt="no!" title="no!"></button>
-    </div>
+	<div class="votecard">
+		<p>Is this a dumpling?</p>
+		<button><img src="/assets/yes.png" alt="yes!" title="yes!" /></button>
+		<button><img src="/assets/no.png" alt="no!" title="no!" /></button>
+	</div>
 </div>
 
-
 <style>
-    .container {
-        position:relative;
-    }
-    .votecard {
-        width:35%;
-        padding:2px 4px 2px 4px;
-        background-color:#FBFCFB;
-        border: 1px #f29f7B solid;
-        position:absolute;
-        display:block;
-        left:75%;
-        top:-65px;
-        z-index:3;
-        font-size:0.7rem;
-        text-align:center;
-    }
+	.container {
+		position: relative;
+	}
+	.votecard {
+		width: 35%;
+		padding: 2px 4px 2px 4px;
+		background-color: #fbfcfb;
+		border: 1px #f29f7b solid;
+		position: absolute;
+		display: block;
+		left: 75%;
+		top: -65px;
+		z-index: 3;
+		font-size: 0.7rem;
+		text-align: center;
+	}
 
-    button :global{
-        font-size:0.6rem;
-        color:#76664A;
-        background-color:transparent;
-        border:none;
-        margin:auto;
-        text-align:center;
-    }
-    button img {
-        max-height:22px;
-    }
-    button:hover :global{
-        filter:drop-shadow(5px 5px 10px #f29f7B);
-    }
-    button:active :global{
-        filter:contrast(80%);
-    }
+	button :global {
+		font-size: 0.6rem;
+		color: #76664a;
+		background-color: transparent;
+		border: none;
+		margin: auto;
+		text-align: center;
+	}
+	button img {
+		max-height: 22px;
+	}
+	button:hover :global {
+		filter: drop-shadow(5px 5px 10px #f29f7b);
+	}
+	button:active :global {
+		filter: contrast(80%);
+	}
 </style>
