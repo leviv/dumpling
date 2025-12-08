23b71851ecccba5297a929c62a30b143fd4f7ddf
Levi Villarreal <villarreallevi@gmail.com>
2025-12-08 01:09:14 -0500

    show flag emoji

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -2,27 +2,31 @@
 	import Card from './Card.svelte';
 	import dumplings from '$lib/dumplings.json';
 	import Voting from './Voting.svelte';
+	import { getCountryFlag } from '../helpers/flagMap';
 
 	export let selectedCountry: string;
 
 	$: dumplingInfo = dumplings.find((d) => d.country === selectedCountry);
+	$: countryFlag = getCountryFlag(selectedCountry);
 	console.log(dumplingInfo);
 </script>
 
 <div class="container">
 	<h1>World Dumpling Index</h1>
 
-	<h3><b>{selectedCountry}</b></h3>
+	<h3><b>{countryFlag} {selectedCountry}</b></h3>
 
 	{#each dumplingInfo?.dumplings as dumpling}
 		<Card {dumpling} />
-		<Voting/>
+		<Voting />
 	{/each}
 </div>
 
 <style>
 	.container {
-		background: #dcd485;
+		position: absolute;
+		top: 0;
+		left: 0;
 		width: 350px;
 		display: flex;
 		flex-direction: column;
